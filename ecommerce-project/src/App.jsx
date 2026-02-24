37af00670b6800b06d762115d6ea4c597b64e53c
Shubham <shubhamsiddharth2001@gmail.com>
2026-02-25 05:11:05 +0530

    create add to cart feature

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,33 +1,37 @@
-import axios from 'axios'
-import { Routes,Route } from 'react-router'
-import { HomePage } from './pages/home/HomePage'
-import { CheckoutPage } from './pages/checkout/CheckoutPage'
-import { OrdersPage } from './pages/orders/OrdersPage'
-import './App.css'
-import { TrackingPage } from './pages/TrackingPage'
-import { NotFoundPage } from './pages/NotFoundPage'
-import { useState, useEffect } from 'react'
-
+import axios from "axios";
+import { Routes, Route } from "react-router";
+import { HomePage } from "./pages/home/HomePage";
+import { CheckoutPage } from "./pages/checkout/CheckoutPage";
+import { OrdersPage } from "./pages/orders/OrdersPage";
+import "./App.css";
+import { TrackingPage } from "./pages/TrackingPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
+import { useState, useEffect } from "react";
 
 function App() {
   const [cart, setCart] = useState([]);
+
+  const loadCart = async () => {
+    const response = await axios.get("/api/cart-items?expand=product");
+    setCart(response.data);
+  };
+
   useEffect(() => {
-    const fetchAppData = async () => {
-      const response = await axios.get('/api/cart-items?expand=product');
-      setCart(response.data);
-    };
-    fetchAppData();
+    loadCart();
   }, []);
 
   return (
     <Routes>
-      <Route index element={<HomePage cart={cart} />} />
+      <Route index element={<HomePage cart={cart} loadCart={loadCart}/>} />
       <Route path="checkout" element={<CheckoutPage cart={cart} />} />
-      <Route path="orders" element={<OrdersPage cart={cart}/>} />
-      <Route path="tracking/:orderId/:productId" element={<TrackingPage cart={cart} />} />
-      <Route path="*" element={<NotFoundPage cart={cart}/>} />
+      <Route path="orders" element={<OrdersPage cart={cart} />} />
+      <Route
+        path="tracking/:orderId/:productId"
+        element={<TrackingPage cart={cart} />}
+      />
+      <Route path="*" element={<NotFoundPage cart={cart} />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
